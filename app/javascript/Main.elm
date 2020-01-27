9bafbbdfb2f8a5eebbccdbd3a965aa7eff703359
veronicacannon <me@veronicacannon.com>
2020-01-26 19:58:47 -0800

    Initial commit

---
+++ app/javascript/Main.elm
@@ -0,0 +1,64 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, Attribute, div, h1, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+-- MODEL
+
+
+type alias Model =
+  { content : String
+  }
+-- INIT
+
+init : Model
+init =
+  { content = "" }
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", value model.content, onInput Change ] []
+    , div [] [ text (slugger model.content) ]
+    ]
+
+-- MESSAGE
+
+type Message
+  = None
+
+-- UPDATE
+type Msg
+  = Change String
+
+
+slugger : String -> String
+slugger name = 
+  name 
+  |> String.toLower
+  |> String.toList
+  |> List.filter Char.isAlpha
+  |> String.fromList
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+-- SUBSCRIPTIONS
+
+-- MAIN
+
+main =
+  Browser.sandbox
+    {
+      init = init,
+      view = view,
+      update = update
+    }
